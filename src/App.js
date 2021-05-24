06b25aa372b1bd4035446b3e96d971d26a633235
Amir Sejdinovic <amir24V@yahoo.com>
2021-05-24 14:04:02 +0200

    Creating the UI interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useState} from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const [crypto,setCryoto] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:"crimson",minHeight:"100vh"}} className="App">
+
+      <h1 className="bg-info p-4">Cryptocurrency Search</h1>
+      <div className="d-flex justify-content-center">
+          <div className="col-md-4 mt-5">
+           <input type="text" className="form-control" value={crypto}  onChange={(e)=>setCryoto(e.target.value)} placeholder="Enter the crpytocurrency" required/>
+
+          </div>
+          
+      </div>
+      <button className="btn btn-secondary px-5 mt-5">Submit</button>
     </div>
   );
 }
